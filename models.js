8f4616f54a55fb66ed5cdc349194918295390447
Manpreet Dhaliwal <manip426@gmail.com>
2021-06-29 21:45:34 -0700

    Troubleshooting
    Troubleshooting

--- models.js
+++ models.js
@@ -26,18 +26,35 @@ let userSchema = mongoose.Schema({
     favorite_movies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 })
 
-// Function that encrypts the password
-userSchema.statics.hashPassword = function(password) {
-    return bcrypt.hashSync(password, 10);
-};
+let genreSchema = mongoose.Schema({
+    name: String,
+    description: String
+})
 
-// Function that compares the encrypted password from the route against the password from the DB.
-userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.pwd);
-};
+let directorSchema = mongoose.Schema({
+  name: String,
+  bio: String,
+  birth: Date,
+  death: Date
+})
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
+
+
+// Function that encrypts the password
+//userSchema.statics.hashPassword = function(password) {
+//    return bcrypt.hashSync(password, 10);
+//};
+
+// Function that compares the encrypted password from the route against the password from the DB.
+//userSchema.methods.validatePassword = function(password) {
+//    return bcrypt.compareSync(password, this.pwd);
+//};
