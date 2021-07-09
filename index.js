02c8ca892b6d856fec446df825fa7fb38aa2d43e
Manpreet Dhaliwal <manip426@gmail.com>
2021-07-08 21:00:06 -0700

    update index.js

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const { check, validationResult } = require('express-validator');
 
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 // Online database connection
-//mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb+srv://Manpreet:Kirat13@manpreetflixdb.rnlcr.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.json());
